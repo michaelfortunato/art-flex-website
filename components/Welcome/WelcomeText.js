0832a81c601aaa55d3d8305eb6f6048242249e9a
Michael Fortunato <michael.n.fortunato@gmail.com>
2021-06-08 23:24:35 -0400

    finished dialogue page

--- components/Welcome/WelcomeText.js
+++ components/Welcome/WelcomeText.js
@@ -6,13 +6,19 @@ import Grid from "@material-ui/core/Grid";
 import styles from "@styles/WelcomeText.module.css";
 import Gridline from "@components/Gridline";
 import theme from "theme";
+
+const animationDuration = 2000;
 const blurbs = [
   {
     text: "Welcome.",
+    enterTime: 1200 || animationDuration,
+    exitTime: 1200 || animationDuration,
     duration: 2000,
   },
   {
     text: "Art Flex is an online store for art, made with artists in mind. Thank you for considering us and our mission. ",
+    enterTime: 1200 || animationDuration,
+    exitTime: 1200 || animationDuration,
     duration: 4000,
   },
   {
@@ -20,16 +26,22 @@ const blurbs = [
       "It is too difficult for artists to generate consistent income from their works. " +
       "Gallieries charge obscene commisions on sales, and they intimiate new buyers and artists alike." +
       "We want to change that.",
+    enterTime: 1200 || animationDuration,
+    exitTime: 1200 || animationDuration,
     duration: 4000,
   },
   {
     text: "We want to democratize how artists find their fans, and how people discover art. ",
+    enterTime: 1200 || animationDuration,
+    exitTime: 1200 || animationDuration,
     duration: 4000,
   },
   {
     text:
       "Art Flex is an online store for art, and we want to do it right. " +
       "We are putting the artist first, with the goal of connecting you to as many buyers as possible, and to growing your popularity through the power of the internet. ",
+    enterTime: 2000 || animationDuration,
+    exitTime: 1200 || animationDuration,
     duration: 4000,
   },
   {
@@ -37,6 +49,8 @@ const blurbs = [
       "We need your help to get this right, and we can use all the advice available. " +
       "Please consider signing up below to become a founding user. " +
       "Thank you.",
+    enterTime: 2000 || animationDuration,
+    exitTime: 1200 || animationDuration,
     duration: 4000,
   },
 ];
@@ -49,7 +63,7 @@ const AnimationContainer = styled.div`
   &.fadeIn-appear-active {
     opacity: 1;
     transition-property: all;
-    transition-duration: ${(props) => props.animationDuration}ms;
+    transition-duration: ${(props) => props.enterTime}ms;
     transition-delay: ${(props) => props.enterDelay}ms;
   }
   &.fadeIn-appear-done {
@@ -61,7 +75,7 @@ const AnimationContainer = styled.div`
   &.fadeIn-enter-active {
     opacity: 1;
     transition-property: all;
-    transition-duration: ${(props) => props.animationDuration}ms;
+    transition-duration: ${(props) => props.enterTime}ms;
     transition-delay: ${(props) => props.enterDelay}ms;
   }
   &.fadeIn-exit {
@@ -70,63 +84,60 @@ const AnimationContainer = styled.div`
   &.fadeIn-exit-active {
     opacity: 0;
     transition-property: all;
-    transition-duration: ${(props) => props.animationDuration}ms;
+    transition-duration: ${(props) => props.exitTime}ms;
     transition-delay: ${(props) => props.exitDelay}ms;
   }
   &.fadeIn-exit-done {
     opacity: 0;
     transition-property: all;
-    transition-duration: ${(props) => props.animationDuration}ms;
-    transition-delay: ${(props) => props.exitDelay}ms;
   }
 `;
 
 export default function WelcomeText(props) {
   const [blurb, setBlurb] = useState(0);
-
-  const animationDuration = 600;
+  const [trigger, setTrigger] = useState(true);
+  useEffect(() => setTrigger(true), [blurb]);
+  const animationDuration = 2000;
   return (
     <div>
       <Grid container className={styles.container}>
         <Grid item xs={12} />
         <Grid item xs={12} style={{ position: "relative" }}>
-          {blurbs.map(({ text, duration }, blurbNumber) => {
-            return (
+          {blurbs.map(
+            ({ text, enterTime, exitTime, duration }, blurbNumber) => (
               <CSSTransition
                 key={blurbNumber}
                 classNames="fadeIn"
                 timeout={{
-                  appear: animationDuration,
-                  enter:
-                    blurbNumber !== 0
-                      ? blurbs[blurbNumber - 1].duration + animationDuration
-                      : animationDuration,
-                  exit: animationDuration + duration,
+                  appear: enterTime,
+                  enter: enterTime,
+                  exit: exitTime,
                 }}
-                onEntered={() => setBlurb(blurb + 1)}
-                onExited={() => {
-                  if (blurb === blurbs.length) {
-                    return props.setCurrentPage(2);
-                  }
-                }}
-                in={blurb == blurbNumber}
+                onEntered={() => setTimeout(() => setTrigger(false), duration)}
+                onExited={() => setBlurb(blurb + 1)}
+                in={blurb === blurbNumber && trigger}
                 appear={blurbNumber === 0}
                 unmountOnExit
               >
                 <AnimationContainer
-                  animationDuration={animationDuration}
-                  enterDelay={
-                    blurbNumber > 0 ? blurbs[blurbNumber - 1].duration : 0
-                  }
-                  exitDelay={duration}
+                  enterTime={enterTime}
+                  enterDelay={0}
+                  exitTime={exitTime}
+                  exitDelay={0}
                 >
-                  <div style={{ "position" : "absolute", "text-align": "center", width: "100%" }}>
+                  <div
+                    style={{
+                      position: "absolute",
+                      "text-align": "center",
+                      width: "100%",
+                    }}
+                  >
                     <Typography variant="h1">{text}</Typography>
                   </div>
                 </AnimationContainer>
               </CSSTransition>
-            );
-          })}
+            )
+          )}
         </Grid>
         <Grid item xs={12} />
       </Grid>
