3e10159ddec22aa923d810fe51955a1833a7d3b5
Michael Fortunato <michael.n.fortunato@gmail.com>
2021-12-07 22:15:40 -0500

    Added URL rewrites when NODE_ENV != dev. Change to NODE_ENV=dev

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
+// Get NODE_ENV="dev" when running npm run dev. Right now it does not.
+const rewrites =
+  process.env.NODE_ENV !== "dev"
+    ? [
+        {
+          source: "/api/:path*",
+          destination: "http://api-gateway.art-flex-apps/:path*"
+        },
+        {
+          source: "/api/account/:path*",
+          destination: "http://account-app.art-flex-apps/:path*"
+        }
+      ]
+    : [];
+
 module.exports = {
   reactStrictMode: true,
   webpack(config) {
@@ -7,7 +22,9 @@ module.exports = {
       issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"]
     });
-
     return config;
+  },
+  async rewrites() {
+    return rewrites;
   }
 };
